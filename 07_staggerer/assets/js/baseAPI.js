74e11062f221b56cae2cfa86c059835243080e09
myc <myc957@126.com>
2021-03-18 23:07:31 +0800

    2021-03-18

--- 07_staggerer/assets/js/baseAPI.js
+++ 07_staggerer/assets/js/baseAPI.js
@@ -4,4 +4,18 @@
 $.ajaxPrefilter(function(options) {
     //在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res) {
+        console.log(res.responseJSON.status);
+        console.log(res.responseJSON.message === '身份认证失败！');
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            window.location = '/07_staggerer/login.html';
+        }
+    }
+
 })
--- 07_staggerer/assets/js/index.js
+++ 07_staggerer/assets/js/index.js
@@ -1,17 +1,53 @@
 $(function() {
     //调用获取用户基本信息方法
     getUserInfo();
+    //设置退出功能
+    var layer = layui.layer;
+    $('#logout').on('click', function() {
+        layer.confirm('是否退出', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            //清除localstorage
+            localStorage.removeItem('token');
+            //跳转页面
+            window.location = '/07_staggerer/login.html';
+
+            layer.close(index);
+        });
+    });
 });
 //获取用户基本信息
 function getUserInfo() {
     $.ajax({
         method: "GET",
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: function(res) {
-            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败');
+            } else {
+                //登录成功，渲染用户头像
+                renderAvatar(res.data);
+            }
+        },
+        error: function() {
+            return layui.layer.msg('调用接口失败');
         }
     });
+}
+//渲染用户头像
+function renderAvatar(data) {
+    var username = data.nickname || data.username;
+    $("#welcome").html('欢迎' + username);
+    if (data.user_pic) {
+        $('.layui-nav-img').attr("src", data.user_pic);
+        $('.text-avatar').hode();
+        $('.layui-nav-img').show();
+    } else {
+        var firstCode = username.substring(0, 1).toUpperCase();
+        $('.layui-nav-img').hide();
+        $('.text-avatar').show();
+        $('.text-avatar').html(firstCode);
+    }
 }
